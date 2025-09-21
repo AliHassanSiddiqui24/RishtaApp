428131e4d14b9efacc79c7521a2196f83388def9
Ali Hassan Siddiqui <alihassansiddiqui24@gmail.com>
2025-09-21 15:06:32 +0500

    Client Side Setup is Done

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+
+ 
+ http = inject(HttpClient);
+ title = 'RishtaApp'
+ users : any
+
+  ngOnInit(): void {
+   this.http.get('https://localhost:5001/api/users').subscribe({
+    next: res => this.users = res,
+    error: err => console.log(err),
+    complete: () => console.log('request has completed')
+   })
+ }
+ 
+}
